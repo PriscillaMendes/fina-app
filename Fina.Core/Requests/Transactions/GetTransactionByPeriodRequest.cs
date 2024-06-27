ac2ad3467ddf618815d1a6ed18994beb9a0c6655
Priscilla <priscilla.carmo@bwtech.com>
2024-06-27 16:59:56 -0300

    Primeira dia da Jornada Fullstack

---
+++ Fina.Core/Requests/Transactions/GetTransactionByPeriodRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core.Requests.Transactions;
+
+public class GetTransactionByPeriodRequest : Request
+{
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set;}
+}
